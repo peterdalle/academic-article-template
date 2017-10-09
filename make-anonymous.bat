9dae78f5c4c1017c93aa4ac88da0d5d77a4e9add
Peter M. Dahlgren <peterdalle@users.noreply.github.com>
2017-10-09 20:10:01 +0200

    Update make-anonymous.bat

--- make-anonymous.bat
+++ make-anonymous.bat
@@ -2,12 +2,11 @@
 if "%1"=="" GOTO MissingFile
 
 :Compile
-echo Making %1-anonymous
+echo Making anonymous %1
 REM Note:
-REM  - ref.bib is the bibliography file in BibTex.
 REM  - apa6.csl is the syntax for APA6 reference formatting.
 REM  - Pandoc variables is explained here: https://pandoc.org/MANUAL.html
-pandoc "%1" -o "%~n1-anonymous.pdf" --bibliography ref.bib --template=svm-latex-anon-ms.tex --csl=apa6.csl --latex-engine=xelatex --number-sections --filter pandoc-citeproc -r markdown+simple_tables+table_captions+yaml_metadata_block -s -S -N -V geometry:margin=1in
+pandoc "%1" -o "%~n1-anonymous.pdf" --template=svm-latex-anon-ms.tex --csl=apa6.csl --latex-engine=xelatex --number-sections --filter pandoc-citeproc -r markdown+simple_tables+table_captions+yaml_metadata_block -s -S -N -V geometry:margin=1in
 GOTO End
 
 :MissingFile
