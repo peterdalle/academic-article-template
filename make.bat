493bcb0d4aa29bdd214b1483664ab509fdfa4d96
Peter M. Dahlgren <peterdalle@users.noreply.github.com>
2017-10-09 19:32:37 +0200

    Create make.bat

---
+++ make.bat
@@ -0,0 +1,17 @@
+@echo off
+if "%1"=="" GOTO MissingFile
+
+:Compile
+echo Making %1
+REM Note:
+REM  - ref.bib is the bibliography file in BibTex.
+REM  - apa6.csl is the syntax for APA6 reference formatting.
+REM  - Pandoc variables is explained here: https://pandoc.org/MANUAL.html
+pandoc "%1" -o "%~n1.pdf" --bibliography ref.bib --csl=apa6.csl --latex-engine=xelatex --number-sections --filter pandoc-citeproc -r markdown+simple_tables+table_captions+yaml_metadata_block -s -S -N -V geometry:margin=1in
+GOTO End
+
+:MissingFile
+echo Use: make [file.md]
+echo    file.md ..... File name of markdown document.
+
+:End
